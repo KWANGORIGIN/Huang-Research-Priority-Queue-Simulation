3b1712147d5d64e644182ae2f284cfd0f834d10e
Kevin Wang <kvw5270@psu.edu>
2020-06-16 19:10:01 -0400

    Design rework of the Timers for better code reusability while simplifying the code and the number of future edits required.

---
+++ SchedulerProgram/src/main/java/QueueInfo/TimeInfoTimer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QueueInfo;
+
+/**
+ *
+ * @author Kevin Wang
+ */
+public class TimeInfoTimer extends CountdownTimer{
+    
+    public TimeInfoTimer(int timeInMinutes){
+        super(timeInMinutes);
+    }
+    
+    public static void main(String[] args){
+//        CountdownWindow timerWindow = new CountdownWindow();
+//        timerWindow.setVisible(true);
+        
+        CountdownTimer testTimer = new TimeInfoTimer(2);
+        testTimer.runCountdownTimer();
+        
+    }
+
+    @Override
+    protected QueueInfo.TimeUpdater createTimeUpdater(int initialTimeInSeconds, int remainingSeconds, CountdownWindow timerWindow) {
+        return new TimeInfoUpdater(initialTimeInSeconds, remainingSeconds, timerWindow);
+    }
+    
+}
