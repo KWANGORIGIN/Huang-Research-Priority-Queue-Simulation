7cb0957c1841f6952dc9f4cfb1910450191d8f36
Kevin Wang <kvw5270@psu.edu>
2020-07-22 10:08:05 -0400

    export_to_excel no works. ThankYouWindow now opens in center

--- SchedulerProgram/src/main/java/ShoppingCart/ThankYouWindow.java
+++ SchedulerProgram/src/main/java/ShoppingCart/ThankYouWindow.java
@@ -66,6 +66,7 @@ public class ThankYouWindow extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
